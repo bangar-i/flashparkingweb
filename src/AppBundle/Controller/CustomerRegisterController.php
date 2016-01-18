68de115fec1de0f8620c4e138eb5049a1a216d1e
Virginie <virginie.gabriel.27@gmail.com>
2016-01-18 14:41:30 +0100

    Premiere vues

---
+++ src/AppBundle/Controller/CustomerRegisterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CustomerRegisterController extends Controller
+{
+    /**
+     * @Route("/customer-register")
+     */
+    public function CustomerRegisterAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/customerRegister/customer-register.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'nav_active' => 'connexion',
+        ));
+    }
+}
