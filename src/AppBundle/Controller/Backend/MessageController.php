c191ff0c3ce8a0e041fca1db33be3f18675077d6
Arthur <aloupfor-htm@outlook.fr>
2016-01-23 05:01:16 +0100

    use backend base

--- src/AppBundle/Controller/Backend/MessageController.php
+++ src/AppBundle/Controller/Backend/MessageController.php
@@ -1,11 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Nani
- * Date: 20/01/2016
- * Time: 21:29
- */
-namespace AppBundle\Controller;
+
+namespace AppBundle\Controller\Backend;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -18,7 +13,7 @@ class MessageController extends Controller
     public function messageAction()
     {
         // replace this example code with whatever you need
-        return $this->render('@App/message/plaintes.html.twig', array(
+        return $this->render('@App/backend/message/plaintes.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
             'nav_active' => 'contact',
         ));
