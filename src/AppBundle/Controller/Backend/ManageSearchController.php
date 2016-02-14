48e7810b9585306a859f00354da01aca8a6a22d9
Nanitebayo <ophelieaboa@otmail.fr>
2016-02-14 16:57:09 +0100

    Ajout page

---
+++ src/AppBundle/Controller/Backend/ManageSearchController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nani
+ * Date: 13/02/2016
+ * Time: 16:52
+ */
+
+namespace AppBundle\Controller\Backend;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ManageSearchController extends Controller
+{
+    /**
+     * @Route("/manage/search")
+     */
+    public function ManageSearchAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/backend/manage/search.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'nav_active' => 'contact',
+        ));
+    }
+}
